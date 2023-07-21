bdc71fd5dbcc9181a7889ffd1336b2aa7de087f4
ralphvw <williamsralph@enyata.com>
2023-07-21 11:16:45 +0000

    Removed git ignore files

--- dist/graphql/resolvers/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const helloResolvers = {
-    Query: {
-        hello: () => 'hello World'
-    }
-};
-exports.default = helloResolvers;
-//# sourceMappingURL=hello.js.map
--- dist/graphql/typedefs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeDefs = `
- type Query {
-    hello: String
- }
-`;
-exports.default = typeDefs;
-//# sourceMappingURL=index.js.map
